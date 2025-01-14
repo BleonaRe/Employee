041093fe695f72b5e260987e0455b052dd09ec50
sofiebajraktari <149413670+sofiebajraktari@users.noreply.github.com>
2025-01-14 18:25:39 +0100

    Created and set views

--- employeeSystem/views.py
+++ employeeSystem/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from employeeSystem.models import (
+    Employee,
+    Attendance,
+    PerformanceReport,
+    Schedule,
+)
+from employeeSystem.serializers import (
+    EmployeeSerializer,
+    AttendanceSerializer,
+    PerformanceReportSerializer,
+    ScheduleSerializer,
+)
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
+
+# ViewSets
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [AllowAny]
+
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [AllowAny]
+
+
+class PerformanceReportViewSet(viewsets.ModelViewSet):
+    queryset = PerformanceReport.objects.all()
+    serializer_class = PerformanceReportSerializer
+    permission_classes = [AllowAny]
+
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+    permission_classes = [AllowAny]
